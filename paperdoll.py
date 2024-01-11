139e72c2c3e2daac1aa6cb56174dfb4fa61ab1f6
Gabriel Autonamo <123652539+autonamoGabs@users.noreply.github.com>
2024-01-11 09:18:04 -0300

    Create paperdoll.py
    PAPER DOLL: Given a string, return a string where for every character in the original there are three characters

---
+++ paperdoll.py
@@ -0,0 +1,16 @@
+
+
+def paper_doll(x):
+    l = list(x)
+    nl = []
+    for k in l:
+        nl.append(k)
+        nl.append(k)
+        nl.append(k)
+    print("".join(nl))
+
+    
+
+
+paper_doll('Hello') 
+paper_doll('Mississippi') 
